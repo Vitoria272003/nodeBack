32a519a1b64c20e55c934c9ee7797515d4087fe0
Vitória Conceição da Silva <vic0253403@aluno.firjansenaisesi.com.br>
2026-03-12 14:09:21 -0300

    segundo comando

--- controller/controller.js
+++ controller/controller.js
@@ -1,5 +1,20 @@
 const express = require("")
 const app = express.Router()
 app.use(exprss.json())
+//rotas
+app.get("/usuario", () => {
+
+})
+
+app.get("/usuario/:id", () => {
+    
+})
+
+app.post("/usuario", () => {})
+
+app.put("/usuario/:id", () => {})
+
+app.delete("/usuario/:id", () => {})
+
 
 module.exports = app
